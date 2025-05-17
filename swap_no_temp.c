6e324a1fb3ba0ad8caa5d4377460d8d1c20cbd3c
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-17 15:46:43 +0100

    commmmit

---
+++ swap_no_temp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int nb1,nb2;
+    scanf("%d %d", &nb1, &nb2);
+    nb1= nb1 + nb2;
+    nb2= nb1 - nb2;
+    nb1= nb1 - nb2;
+    printf("Après échange a = %d, b = %d\n", nb1, nb2);
+    return 0;
+}
